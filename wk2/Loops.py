4b0e5f7330c917f3ce130aadd14184119e3b89cf
Yifan Cao <9102602+sunnyifan@users.noreply.github.com>
2018-03-06 23:57:31 -0500

    loops: cont'd

--- wk2/Loops.py
+++ wk2/Loops.py
@@ -2,7 +2,7 @@
 def sumFromMToN(m, n):
     total = 0
     # note that range(x, y) includes x but excludes y
-    for x in range(m, n+1):     # m inlcusive, n+1 exclusive
+    for x in range(m, n+1):     
         total += x
     return total
 
@@ -14,12 +14,8 @@ def sumFromMToN(m, n):
 
 print(sumFromMToN(5, 10) == 5+6+7+8+9+10)
 
-# And we can even do this with a closed-form formula,
-# which is the fastest way, but which doesn't really
-# help us demonstrate loops.  :-)
-
-def sumToN(n):
-    # helper function
+# And we can even do this with a closed-form formula
+def sumToN(n):              # helper function
     return n*(n+1)//2
 
 def sumFromMToN_byFormula(m, n):
@@ -44,3 +40,49 @@ def sumEveryKthFromMToN(m, n, k):
     return total
 
 print(sumEveryKthFromMToN(5, 20, 7) == (5 + 12 + 19))
+
+# Sum just odd numbers from m to n:
+def sumOfOddsFromMToN(m, n):
+    total = 0
+    for x in range(m, n+1):
+        if (x % 2 == 1):
+            total += x
+    return total
+
+print(sumOfOddsFromMToN(4, 10) == sumOfOddsFromMToN(5,9) == (5+7+9))
+
+# Once again:
+def sumOfOddsFromMToN(m, n):
+    if (m % 2 == 0):
+        # m is even, add 1 to start on an odd
+        m += 1
+    total = 0
+    for x in range(m, n+1, 2):
+        total += x
+    return total
+
+print(sumOfOddsFromMToN(4, 10) == sumOfOddsFromMToN(5,9) == (5+7+9))
+
+# Here we will range in reverse (not wise in this case, but instructional)
+def sumOfOddsFromMToN(m, n):
+    if (n % 2 == 0):
+        # n is even, subtract 1 to start on an odd
+        n -= 1
+    total = 0
+    for x in range(n, m-1, -2):  # be careful here!
+        total += x
+    return total
+
+print(sumOfOddsFromMToN(4, 10) == sumOfOddsFromMToN(5,9) == (5+7+9))
+
+def sumOfOddsFromMToN(m, n):
+    if (m % 2 == 0): m += 1
+    return sum(range(m, n+1, 2))
+
+print(sumOfOddsFromMToN(4, 10) == sumOfOddsFromMToN(5,9) == (5+7+9))
+
+# This is the worst way so far -- too confusing.
+def sumOfOddsFromMToN(m, n):
+    return sum(range(m + (1 - m%2), n+1, 2)) # this works, but it's gross!
+
+print(sumOfOddsFromMToN(4, 10) == sumOfOddsFromMToN(5,9) == (5+7+9))
