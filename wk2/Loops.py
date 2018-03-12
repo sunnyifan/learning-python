096013dce10d35f665dd959d82befc64c59e310d
Yifan Cao <9102602+sunnyifan@users.noreply.github.com>
2018-03-11 23:58:57 -0400

    loops: up to nthPrime

--- wk2/Loops.py
+++ wk2/Loops.py
@@ -224,3 +224,66 @@ for n in range(100):
     if isPrime(n):
         print(n, end=" ")
 print()
+
+# fasterIsPrime
+#Note: this is still not the fastest way, but it's a nice improvement.
+def fasterIsPrime(n):
+    if (n < 2):
+        return False
+    if (n == 2):
+        return True
+    if (n % 2 == 0):
+        return False
+    maxFactor = round(n**0.5)
+    for factor in range(3,maxFactor+1,2):
+        if (n % factor == 0):
+            return False
+    return True
+
+# And try out this version:
+for n in range(100):
+    if fasterIsPrime(n):
+        print(n, end=" ")
+print()
+
+# Verify fasterIsPrime is faster:
+def isPrime(n):
+    if (n < 2):
+        return False
+    for factor in range(2,n):
+        if (n % factor == 0):
+            return False
+    return True
+
+def fasterIsPrime(n):
+    if (n < 2):
+        return False
+    if (n == 2):
+        return True
+    if (n % 2 == 0):
+        return False
+    maxFactor = round(n**0.5)
+    for factor in range(3,maxFactor+1,2):
+        if (n % factor == 0):
+            return False
+    return True
+
+# Verify these are the same
+for n in range(100):
+    assert(isPrime(n) == fasterIsPrime(n))
+print("They seem to work the same!")
+
+# Now let's see if we really sped things up
+import time
+bigPrime = 499 # Try 1010809, or 10101023, or 102030407
+print("Timing isPrime(",bigPrime,")", end=" ")
+time0 = time.time()
+print(", returns ", isPrime(bigPrime), end=" ")
+time1 = time.time()
+print(", time = ",(time1-time0)*1000,"ms")
+
+print("Timing fasterIsPrime(",bigPrime,")", end=" ")
+time0 = time.time()
+print(", returns ", fasterIsPrime(bigPrime), end=" ")
+time1 = time.time()
+print(", time = ",(time1-time0)*1000,"ms")
