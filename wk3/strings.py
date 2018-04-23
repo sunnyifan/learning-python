5a4299d907b406db0d7cffc76f10e6a654917c2a
Yifan Cao <9102602+sunnyifan@users.noreply.github.com>
2018-04-22 23:59:22 -0400

    Complete String literals as multi-line comments

--- wk3/strings.py
+++ wk3/strings.py
@@ -10,7 +10,7 @@ print("""triple double-quotes""")
 # 2. New lines in strings
 print("abc\ndef")  # \n is a single newline character
 print("""abc
-def""")
+def""")            # '''(\) start a new line '''
 
 print("""\
 You can use a backslash at the end of a line in a string to exclude
@@ -18,3 +18,35 @@ the newline after it. This should almost never be used, but one good
 use of it is in this example, at the start of a multi-line string, so
 the whole string can be entered with the same indentation (none, that is).
 """)
+
+# 3. More escape sequences
+print("Double-quote: \"")
+print("Backslash: \\")
+print("Newline (in brackets): [\n]")
+print("Tab (in brackets): [\t]")        # \t for tab
+
+print("These items are tab-delimited, 3-per-line:")
+print("abc\tdef\tg\nhi\tj\\\tk\n---")
+
+# an escape sequence produces a single character
+s = "a\\b\"c\td"
+print("s =", s)
+print("len(s) =", len(s))       # len for length
+
+s = "a\\b\"cd"
+print("s =", s)
+print("len(s) =", len(s))       # len(\t) = 1
+
+# 4. Concatenated Literals
+s = 'abc' 'def'         # ok (but "abc" + "def" is preferred)
+print(s)
+# error: s = s 'def'(only works with string literals, not variables)
+
+# 5. String literals as multi-line comments
+"""
+Python does not have multiline comments, but you can do something similar
+by using a top-level multiline string, such as this. Technically, this is
+not a comment, and Python will evaluate this string, but then ignore it
+and garbage collect it!
+"""
+print("wow!")
